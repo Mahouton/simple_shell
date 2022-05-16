fda3eb894384a38d148fb6f8cc432776abf011d7
Paul KPADONOU <pmkpaul87@gmail.com>
2022-05-16 18:28:56 +0100

    Fix Betty style error

--- search_cwd.c
+++ search_cwd.c
@@ -1,7 +1,10 @@
 #include "shell.h"
+
 /**
  * search_cwd - look for current working dir
  * @filename: file name
+ * @er: current working dir
+ *
  * Return: current working dir
  */
 char *search_cwd(char *filename, char *er)
@@ -34,7 +37,7 @@ char *search_cwd(char *filename, char *er)
 				closedir(dir);
 				if (!(access(ret, X_OK)))
 					return (ret);
-				else
+				/*else*/
 					write(2, er, 5);
 			}
 		}
